681968201e7e00408a35f8789945c9d6148f7160
azzaa-tech <chewscoups@gmail.com>
2025-10-07 21:18:27 +0800

    update .gitignore agar repo lebih bersih

--- .next/static/chunks/src_app_anjayy_page_45a68a45.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f52fcb10._.js"
-],
-    source: "dynamic"
-});
